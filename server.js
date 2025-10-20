a1a4035d66dfe9dbac8f464432e7a17937c3d3b7
TonosakiRen <ren221603@gmail.com>
2025-10-20 13:07:25 +0900

    port

--- server.js
+++ server.js
@@ -53,6 +53,8 @@ function getLeaderboard() {
     .slice(0, 10);
 }
 
-httpServer.listen(3000, "0.0.0.0", () => {
-  console.log("✅ Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+httpServer.listen(PORT, "0.0.0.0", () => {
+    console.log(`✅ Server running on port ${PORT}`);
 });
